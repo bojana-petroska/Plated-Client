7226349356d16f07b77fcecaa34f42017a62e8ad
bojana-petroska <bojana.petroska@yahoo.com>
2025-01-26 19:15:35 +0100

    adjust Buttons Component

--- src/components/Buttons.tsx
+++ src/components/Buttons.tsx
@@ -10,7 +10,7 @@ interface ButtonProps {
 
 const Button: React.FC<ButtonProps> = ({ onClick, text, type, size = 'default', children }) => {
   const baseStyles = 'px-24 py-3 rounded-[15px] transition duration-300';
-  const sizeStyles = size === 'small' ? 'px-4 py-1' : 'px-24 py-3';
+  const sizeStyles = size === 'small' ? 'px-4 py-1' : 'w-full px-4 py-3 border rounded-[15px] focus:ring-2 focus:ring-pink-400';
   const whiteButtonStyles = 'bg-white text-black hover:bg-gray-100';
   const pinkButtonStyles = 'bg-[#FF7F7F] text-white hover:bg-[#FF6B6B]';
 
